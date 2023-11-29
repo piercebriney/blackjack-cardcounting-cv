1e870069ecd75ce720ba073f3c133d3fae8cebfa
Nathan Nguyen <nathan.tm.nguyen@gmail.com>
2023-11-29 02:02:29 -0600

    save

--- end2end/include/config.h
+++ end2end/include/config.h
@@ -6,6 +6,7 @@
 constexpr int NUM_LABELS = 52;
 const std::vector<std::string> CLASS_NAMES = {"10C", "10D", "10H", "10S", "2C", "2D", "2H", "2S", "3C", "3D", "3H", "3S", "4C", "4D", "4H", "4S", "5C", "5D", "5H", "5S", "6C", "6D", "6H", "6S", "7C", "7D", "7H", "7S", "8C", "8D", "8H", "8S", "9C", "9D", "9H", "9S", "AC", "AD", "AH", "AS", "JC", "JD", "JH", "JS", "KC", "KD", "KH", "KS", "QC", "QD", "QH", "QS"};
 cv::Size size{640, 640};
+static card LABEL_TO_CARD[] = {_AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS, _AS};
 #else
 cv::Size size{1280, 1280};
 constexpr int NUM_LABELS = 13;
