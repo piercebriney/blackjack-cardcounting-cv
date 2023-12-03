3d25d8a116b62a7646c7d15c3006794b4fef0930
SaketG1 <65042050+SaketG1@users.noreply.github.com>
2023-12-02 18:59:43 -0600

    use maps for strategy lookup (#44)

--- simulator/strategy.h
+++ simulator/strategy.h
@@ -3,16 +3,22 @@
 #include "common.h"
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 typedef std::vector<std::vector<std::string>> table;
+typedef std::unordered_map<std::string, std::string> mp;
 
 extern table g_hardTotalsTable;
 extern table g_softTotalsTable;
 extern table g_pairSplittingTable;
 extern table g_lateSurrenderTable;
+extern mp g_hardTotalsMap;
+extern mp g_softTotalsMap;
+extern mp g_pairSplittingMap;
+extern mp g_lateSurrenderMap;
 extern std::vector<deviation> g_illustrious_18;
 
-std::string lookup(table table, std::string column, std::string row);
+std::string lookup(table table, std::string column, std::string row, std::string tableName);
 
 void loadStrategy();
 
